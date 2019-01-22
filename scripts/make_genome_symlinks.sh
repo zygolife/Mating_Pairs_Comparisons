554f134fb73f8c801042b5a23d2196b64f6bb36b
Jason Stajich <jasonstajich.phd@gmail.com>
2019-01-21 21:22:01 -0800

    make links

--- scripts/make_genome_symlinks.sh
+++ scripts/make_genome_symlinks.sh
@@ -2,7 +2,9 @@
 
 FILE=strain_pairs.dat
 LCGFOLDER=/bigdata/stajichlab/shared/projects/ZyGoLife/LCG/Annotation/genomes/
-TARGETFOLDER=GENOMES
+TARGETFOLDER=genomes
+
+mkdir -p $TARGETFOLDER
 IFS=,
 tail -n +2 $FILE | while read NAME PLUS MINUS
 do
@@ -11,8 +13,8 @@ do
 
 	for n in $PLUS $MINUS
 	do
-		if [ ! -f genomes/$n.fasta ]; then
-			ln -s $LCGFOLDER/$n.sorted.fasta $TARGET/$n.fasta
+		if [ ! -f $TARGETFOLDER/$n.fasta ]; then
+			ln -s $LCGFOLDER/$n.sorted.fasta $TARGETFOLDER/$n.fasta
 #			ln -s $LCGFOLDER/$n.masked.fasta $TARGET/$n.masked.fasta
 		fi
 	done
